a34b4eb91644ffd65da4516c4e67eb57d56927ea
bhumip214 <bhumip214@gmail.com>
2019-03-06 13:18:44 -0800

    controls tests done

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,26 @@
-// Test away!
+import React from "react";
+import Control from "./Controls";
+import { render, cleanup } from "react-testing-library";
+import "jest-dom/extend-expect";
+
+afterEach(cleanup);
+
+describe("<Control />", () => {
+  it("should render both buttons when gate is closed and locked", () => {
+    const { getByTestId } = render(<Control locked={true} closed={true} />);
+    const lockedBtn = getByTestId("locked-btn");
+    const closedBtn = getByTestId("closed-btn");
+
+    expect(lockedBtn).toHaveTextContent("Unlock Gate");
+    expect(closedBtn).toHaveTextContent("Open Gate");
+  });
+
+  it("should render both buttons when gate is open and unlock", () => {
+    const { getByTestId } = render(<Control locked={false} closed={false} />);
+    const lockedBtn = getByTestId("locked-btn");
+    const closedBtn = getByTestId("closed-btn");
+
+    expect(lockedBtn).toHaveTextContent("Lock Gate");
+    expect(closedBtn).toHaveTextContent("Close Gate");
+  });
+});
